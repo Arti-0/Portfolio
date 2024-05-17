ed59d040cc7dd5cf39e3a284a351e0d1a4495f7e
Arti <andreasbodin.pro@gmail.com>
2024-05-17 14:56:09 +0200

    DeployT1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Portfolio/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+
+        }
+      }
+    },
+    chunkSizeWarningLimit: 600
+  },
   plugins: [react()],
 })
